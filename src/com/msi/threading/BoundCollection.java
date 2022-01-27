a6a2bc51f7e034bbac1e1bd8358fe15a45ac8651
yeganeh <yeganeh@behsazan.net>
2022-01-27 13:53:54 +0330

    An example using semaphore.

---
+++ src/com/msi/threading/BoundCollection.java
@@ -0,0 +1,67 @@
+package com.msi.threading;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+public class BoundCollection {
+    private final Semaphore semaphore;
+    private List<Integer> arrayList;
+
+    public BoundCollection(int limit) {
+        semaphore = new Semaphore(limit);
+        this.arrayList = Collections.synchronizedList(new ArrayList<>());
+    }
+
+    public boolean add(Integer i) throws InterruptedException {
+        boolean added = false;
+        semaphore.acquire();
+        added = arrayList.add(i);
+        if (!added)
+            semaphore.release();
+        return added;
+    }
+
+    public boolean remove(Integer i) {
+        boolean removed = arrayList.remove(i);
+        if (removed)
+            semaphore.release();
+        return removed;
+    }
+
+    public static void main(String[] args) {
+        final BoundCollection boundCollection = new BoundCollection(10);
+
+        ExecutorService service = Executors.newFixedThreadPool(1);
+
+        new Thread(() -> {
+            for (int i = 0; i < 20; i++) {
+                try {
+                    boundCollection.add(i);
+                    System.out.println(Thread.currentThread().getName() + " adding value: " + i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        ;
+
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            for (int i = 0; i < 20; i++) {
+                if (boundCollection.remove(i)) {
+                    System.out.println(Thread.currentThread().getName() + " remobing value: " + i);
+                }
+            }
+        }).start();
+
+    }
+}
