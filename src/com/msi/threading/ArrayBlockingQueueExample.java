6f33b8b420c1cfeb38f8a31e61aa103fc80c1914
yeganeh <yeganeh@behsazan.net>
2022-02-14 14:56:49 +0330

    ArrayBlockingQueueExample.

---
+++ src/com/msi/threading/ArrayBlockingQueueExample.java
@@ -0,0 +1,48 @@
+package com.msi.threading;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ArrayBlockingQueueExample {
+    public static void main(String[] args) {
+        ArrayBlockingQueue<Integer> arrayBlockingQueue = new ArrayBlockingQueue<>(5);
+
+        Runnable producer = () -> {
+            int i=0;
+            while (true){
+                try {
+                    arrayBlockingQueue.put(++i);
+                    System.out.println("Added: " + i);
+                    TimeUnit.MILLISECONDS.sleep(20);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Runnable consumer = () -> {
+            while (true){
+                try {
+                    TimeUnit.MILLISECONDS.sleep(10000);
+                    Integer poll;
+                    poll = arrayBlockingQueue.take();
+                    System.out.println("Polled: " + poll);
+                    TimeUnit.MILLISECONDS.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+
+
+        ExecutorService service = Executors.newFixedThreadPool(2);
+
+        service.submit(producer);
+        service.submit(consumer);
+
+        service.shutdown();
+    }
+}
